33fcd5e7391014dd5162abbffba19acdb300dfe2
Maye85 <mayegabrielaboffil@gmail.com>
2024-07-30 15:58:34 -0500

    arreglo del test JS con el cambio al metodo map y reduce, agregamos el readme y comentarios del código para mejor comprension del mismo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import {characters} from './data/dataset.js';
 import { renderItems } from './view.js';
 import { calcularEstadisticas, getCharacters, filterCharactersByFamily, sortCharactersByName } from './dataFunctions.js';
 //se agrega calcularEstadicticas en el import de dataFuntion
+
 function init() {
+  //se encarga de inicializar la aplicación,se configuran los manejadores de eventos y se realizan acciones iniciales
   const familyFilter = document.querySelector('#family-filter');
   const sortFilter = document.querySelector('#alfabetico');
   const clearButton = document.querySelector('[data-testid="button-clear"]');
@@ -10,39 +12,52 @@ function init() {
   const resultadoEstadisticas = document.querySelector('#resultado-estadisticas');
 
   resultadoEstadisticas.style.display = 'none';
-  // busca la data sino que la prepara para ser usada
+  // inicialmente oculta el contenedor de resultados estadisticos
   renderItems(getCharacters());
+  // trae los personajes y el renderItems crea una lista para mostrar 
 
   familyFilter.addEventListener('change', () => {
+    //(escucha el change)se espera el cambio de familia para comenzar la funcion 
     const filteredCharacters = filterCharactersByFamily(familyFilter.value);
+    //se selecciona la familia 
     renderItems(filteredCharacters);
+    //renderizamos filteredCharacters y generamos elementos HTML dinámicamente 
   });
 
+  //ordemanos dentro del filtro
   sortFilter.addEventListener('change', () => {
+    //(escucha el change)se espera el cambio para comenzar la funcion 
     const filteredCharacters = filterCharactersByFamily(familyFilter.value);
+    //se selecciona la familia 
     let datafilter;
+    //prepara la variable para almacenar un valor
     if (familyFilter.value=== ""){
-      console.log (familyFilter.value)
+      //si no se selecciona ninguna familia solo se ordena por nombre
+      
       datafilter = sortCharactersByName(characters, "name", sortFilter.value);
-      console.log (datafilter)
+    //ordenamos los datos para prepararlos para el uso del DOM
     }
     else{
       datafilter = sortCharactersByName(filteredCharacters, "name", sortFilter.value);
-    }
+    } //ordena solo los personajes que ya han pasado por el filtro de familia.
     renderItems(datafilter)
+    //toma la lista de personajes y actualiza la interfaz de usuario
+    //para ser mostrada en la interfaz de usuario
   });
 
   clearButton.addEventListener('click', () => {
-    // Restablecer los filtros
+    // Restablecer el filtrado y ordenado
     familyFilter.value = '';
     sortFilter.value = '';
 
+    renderItems(getCharacters()); 
     // Renderizar todos los personajes nuevamente sin aplicar filtros adicionales
-    renderItems(getCharacters()); // Asegúrate de que getCharacters() devuelva todos los personajes
   });
-  //creamos el metodo addEventListener y el manejador de evento clic
+  
   botonCalcularEstadisticas.addEventListener('click', () => {
+    //creamos el metodo addEventListener y el manejador de evento click
     const stats = calcularEstadisticas(getCharacters());
+    //traemos los personajes, para calcular la estadisticas y para luego guardarlos en stats
     resultadoEstadisticas.innerHTML = `
       <p>Casados: ${stats.casados}</p>
       <p>Solteros: ${stats.solteros}</p>
@@ -50,8 +65,10 @@ function init() {
       <p>Amantes: ${stats.amantes}</p>
     `;
     // Mostrar el contenedor de resultados
-    resultadoEstadisticas.style.display = 'flex'; // Mostrar el contenedor
+    resultadoEstadisticas.style.display = 'flex'; 
+    // Mostrar el contenedor
   });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+//nos aseguramos que init inicie una vez cargue el HTML para eviar errores
